26f711cc8a2512ae3005626bc68215efb400aed1
rowaidaahmady <rowaida.ahmadi@gmail.com>
2023-10-07 00:16:00 +0300

    16+18
    add a medicine with its details (active ingredients) , price and available quantity
    
    edit medicine details and price

--- src/Routes/pharmacistController.js
+++ src/Routes/pharmacistController.js
@@ -1,5 +1,6 @@
 const pharmacistModel = require("../Models/Pharmacist");
 const PharmacistRegisterRequestModel = require("../Models/PharmacistRegisterRequest");
+const MedicineModel = require('../Models/Medicine');
 const { default: mongoose } = require("mongoose");
 
 //---------------------------------------REGISTRATION REQUEST-----------------------------------------------
@@ -15,16 +16,35 @@ const registerPharmacist = async (req, res) => {
 
 //------------------------------------MEDICINE------------------------------------
 
-const addMedicine = async (req, res) => {
-    //Req. 16 code here
+const addMedicine = async (req,res) => {
+  try {
+      const newMedicine = await MedicineModel.create(req.body);
+      res.status(201).json(newMedicine);
+  }catch(error){
+      res.status(400).json({ error: error.message });
+  }
 }
 
-const updateMedicine = async (req, res) => {
-    //Req. 18 code here
+module.exports = {addMedicine};
+
+const updateMedicine = async (req,res) => {
+  const id = req.body.id;
+  const updates = req.body;
+  try{
+      const updateMedicine = await MedicineModel.findByIdAndUpdate(id, updates);
+      if(!updateMedicine){
+          return res.status(404).json({error: "Medicine not found "});
+      }
+      res.status(200).json(updateMedicine);
+  }catch(error) { 
+      res.status(500).json({error: error.message});
+  }
 }
 
 //---------------------------------------EXPORTS-----------------------------------------------
 
 module.exports = {
   registerPharmacist,
+  addMedicine,
+  updateMedicine
 };
