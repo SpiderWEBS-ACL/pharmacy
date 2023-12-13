0b742f0923cbfe8d659712f208fed681d20586a4
marwantam <marwan.tam18@gmail.com>
2023-12-13 18:14:23 +0200

    orders sorted

--- client/src/pages/Patient/ViewAllOrders.tsx
+++ client/src/pages/Patient/ViewAllOrders.tsx
@@ -11,7 +11,6 @@ import {
   DeleteOutlined
 } from "@ant-design/icons";
 
-
 const ViewAllOrders: React.FC = () => {
   const accessToken = Cookies.get("accessToken");
   let id = "";
@@ -20,8 +19,7 @@ const ViewAllOrders: React.FC = () => {
     id = decodedToken.id as string;
   }
 
-  console.log(id);
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState<any[]>([]);
   const [date, setDate] = useState<string>("");
   const [loading, setLoading] = useState(true);
   const [flag, setFlag] = useState(true);
@@ -41,75 +39,70 @@ const ViewAllOrders: React.FC = () => {
       .catch((error) => {
         console.error("Error:", error);
       });
-
   }, []);
 
-  async function getDate(date: string) {
-    var dateObj = new Date(date.split("T")[0]);
-    console.log(dateObj);
-    setDate(dateObj.getDate().toString());
-  }
+  // Function to check if the order is processing
+  const isProcessing = (order: any) => order.Status === "Processing";
 
-  if (loading) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Spin size="large" />
-      </div>
-    );
-  }
+  // Sort orders, processing orders first, then by date
+  const sortedOrders = [...orders].sort((a, b) => {
+    if (isProcessing(a) && !isProcessing(b)) {
+      return -1;
+    } else if (!isProcessing(a) && isProcessing(b)) {
+      return 1;
+    } else {
+      // Sort by date if both orders have the same status
+      return new Date(b.Date).getTime() - new Date(a.Date).getTime();
+    }
+  });
 
   return (
     <div className="container">
       <h2 className="text-center mt-4 mb-4">
-        {" "}
-        <strong>Your Orders</strong>{" "}
+        <strong>Your Orders</strong>
       </h2>
 
-        {orders.map((order: any) => (
-            <div>
-
-                <Card title={`Order #${order._id}`} style={{lineHeight: 2}} onClick={()=> {navigate(`/patient/viewOrder/${order._id}`)}} hoverable={true}>
-                    <Row>
-                        <Col style={{marginRight: 40}} span={10}>
-                    <b>Placed On: </b>{(new Date(order.Date.split("T")[0])).toDateString()} <br />
-                    <b>Shipping Address: </b> {order.DeliveryAddress}
-                    </Col>
-                    <Col span={8}>
-                    <b>Total: </b> {order.TotalPrice} USD <br />
-                    <b>Payment Method: </b> {order.PaymentMethod}
-                    </Col>
-                    <Col span= {4} style={{float: "right", textAlign: "right", verticalAlign: "middle"}} >
-                    <b>Order Status: </b>
-                    <span
-                        style={{
-                        color:
-                            order.Status == "Cancelled"
-                            ? "red"
-                            : order.Status == "Shipped"
-                            ? "green"
-                            : "black",
-                        }}
-                    >
-                        {order.Status}
-                    </span>
-                        <br />
-                    {/* <button className="btn btn-primary" onClick={()=> {navigate(`/patient/viewOrder/${order._id}`)}} style={{marginTop: 5}}>
-                        View Details
-                    </button> */}
-
-                    </Col>
-                    </Row>
-                </Card>
+      {sortedOrders.map((order: any) => (
+        <div key={order._id}>
+          <Card
+            title={`Order #${order._id}`}
+            style={{ lineHeight: 2 }}
+            onClick={() => {
+              navigate(`/patient/viewOrder/${order._id}`);
+            }}
+            hoverable={true}
+          >
+            <Row>
+              <Col style={{ marginRight: 40 }} span={10}>
+                <b>Placed On: </b>
+                {new Date(order.Date.split("T")[0]).toDateString()} <br />
+                <b>Shipping Address: </b> {order.DeliveryAddress}
+              </Col>
+              <Col span={8}>
+                <b>Total: </b> {order.TotalPrice} USD <br />
+                <b>Payment Method: </b> {order.PaymentMethod}
+              </Col>
+              <Col span={4} style={{ float: "right", textAlign: "right", verticalAlign: "middle" }}>
+                <b>Order Status: </b>
+                <span
+                  style={{
+                    color:
+                      order.Status === "Cancelled"
+                        ? "red"
+                        : order.Status === "Shipped"
+                        ? "green"
+                        : "black",
+                  }}
+                >
+                  {order.Status}
+                </span>
                 <br />
-            </div>
-        ))}
+              </Col>
+            </Row>
+          </Card>
+          <br />
+        </div>
+      ))}
     </div>
   );
 };
